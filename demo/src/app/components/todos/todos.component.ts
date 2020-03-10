307fc3a2aee217b9d3b56d71a54de6651c7b7e18
EasonLu <EasonLu@Yi-Hsiens-iMac.local>
2020-03-09 22:51:21 -0400

    completed demo

--- demo/src/app/components/todos/todos.component.ts
+++ demo/src/app/components/todos/todos.component.ts
@@ -26,4 +26,10 @@ export class TodosComponent implements OnInit {
     this.todoService.deleteTodo(todo).subscribe();
   }
 
+  addTodo(todo:Todo): void {
+    this.todoService.addTodo(todo).subscribe(todo=>{
+      this.todos.push(todo);
+    });
+  }
+
 }
